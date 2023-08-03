3fa4f22c0eccc53f2d7782328c1200043a71a56d
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-08-03 16:21:29 +0530

    ss

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
 echo -e "\e[33mCopy Mongodb Repo File\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling Mongodb Server\e[0m"
+echo -e "\e[33mInstalling Mongodb Server\e[0m" &>>/tmp/roboshop.log
 yum install mongodb-org -y 
 
 ##we need to copy confi file 
 
 echo -e "\e[33mStart Mongodb Service\e[0m"
-systemctl enable mongod 
-systemctl restart mongod 
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
