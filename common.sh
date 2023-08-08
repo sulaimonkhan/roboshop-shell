9b92bf7a1c1748afe833c516fa770ac7c406809b
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-08-08 18:50:56 +0530

    catalogue

--- common.sh
+++ common.sh
@@ -2,6 +2,11 @@ color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+user_id=$(id -u)
+if [ $user_id -ne 0 ]; then
+  echo scrpit should be running with sudo
+  exit 1
+fi  
 
 stat_check() {
   if [ $1 -eq 0 ]; then
