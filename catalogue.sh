d45240cb44798a4961622b675d64878d1fcf096b
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-12 12:51:05 +0530

    catalogue scrpting

---
+++ catalogue.sh
@@ -0,0 +1,23 @@
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+dnf install mongodb-mongosh -y
+
+mongosh --host 172.31.20.140 </app/db/master-data.js
