801c43688bf7608edcd978c1452549dd07ee70fd
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-12 18:40:40 +0530

    mongodb sed update command

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 dnf install mongodb-org -y
 
-
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
