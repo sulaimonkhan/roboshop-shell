365df2f6b450cd0ff63b341f632e880b37c98ef1
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-12 23:23:30 +0530

    payment.rabbitmq,dispatch i am sending three shell scripting

---
+++ rabbitmq.sh
@@ -0,0 +1,9 @@
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+
+dnf install rabbitmq-server -y
+
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
